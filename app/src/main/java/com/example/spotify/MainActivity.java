6348b32fe47ef2f2a677d1ebbafb64d159314b16
estherVS <esther.vieirabv@gmail.com>
2020-10-23 10:03:34 -0400

    "play music"

--- app/src/main/java/com/example/spotify/MainActivity.java
+++ app/src/main/java/com/example/spotify/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.spotify;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
@@ -11,15 +13,26 @@ import com.bumptech.glide.Glide;
 public class MainActivity extends AppCompatActivity {
 
     ImageView imagem;
+    MediaPlayer player;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
          imagem = (ImageView) findViewById(R.id.imageView);
-
         Glide.with(this).load("https://img.youtube.com/vi/j7zBBM6z4E4/0.jpg").into(imagem);
+
+        player=MediaPlayer.create(getApplicationContext(), R.raw.clocks);
     }
 
+    protected void onResume(){
+        super.onResume();
     }
+    public void player(View view) {
+        player.start();
+    }
+}
 
